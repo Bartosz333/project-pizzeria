9cd7a529b6aff9d91eb8baa126c9bc23c3a80b3b
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-04-14 12:23:12 +0200

    Add class Cart

--- src/js/script.js
+++ src/js/script.js
@@ -103,7 +103,8 @@
 
     getElements(){
       const thisProduct = this;
-    
+      //thisProduct.dom = {};
+      
       thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
       //console.log(thisProduct.accordionTrigger);
       thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form);
@@ -290,6 +291,38 @@
     }
   }
 
+  class Cart {
+    constructor(element) {
+      const thisCart = this;
+
+      thisCart.products = [];
+
+      thisCart.getElements(element);
+      thisCart.initActions();
+
+      console.log('new Cart', thisCart);
+    }
+
+    getElements(element) {
+      const thisCart = this;
+
+      thisCart.dom = {};
+
+      thisCart.dom.wrapper = element;
+      thisCart.dom.toggleTrigger = thisCart.dom.wrapper.querySelector(select.cart.toggleTrigger);
+    }
+
+    initActions() {
+      const thisCart = this;
+
+      /** 'click' event listener on thisCart.dom.toggleTrigger*/
+      thisCart.dom.toggleTrigger.addEventListener('click', function() {
+        // The handler of this listener is to toggle the class
+        thisCart.dom.wrapper.classList.toggle(classNames.cart.wrapperActive);
+      });
+    }
+  }
+
   const app = {
     initMenu: function(){
       const thisApp = this;
@@ -299,11 +332,20 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
+
     initData: function(){
       const thisApp = this;
 
       thisApp.data = dataSource;
     },
+
+    initCart: function() {
+      const thisApp = this;
+
+      const cartElem = document.querySelector(select.containerOf.cart);
+      thisApp.cart = new Cart(cartElem);
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -314,6 +356,7 @@
 
       thisApp.initData();
       thisApp.initMenu();
+      thisApp.initCart();
     },
   };
 
