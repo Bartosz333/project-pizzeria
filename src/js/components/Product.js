ac18ee709edc1d3037d0c446c731cf4aedf969b5
Bartosz Kozaczkiewicz <bkozaczkiewicz@hotmail.com>
2023-04-27 18:25:45 +0200

    Add corect

--- src/js/components/Product.js
+++ src/js/components/Product.js
@@ -184,7 +184,7 @@ class Product {
     const event = new CustomEvent('add-to-cart', {
       bubbles: true,
       detail: {
-        product: thisProduct,
+        product: thisProduct.prepareCartProduct()//calling a function that returns productSumary***
       },
     });
 
